501b8627cec3a59d6ac30055e4a17a787cb06e8b
rehan7594 <rehansdm94@gmail.com>
2021-05-30 19:33:44 -0700

    Added:Code to add new contact with validation

--- src/com/addressbook/Address_Book.java
+++ src/com/addressbook/Address_Book.java
@@ -1,14 +1,15 @@
 package com.addressbook;
 import java.util.*;
+import java.util.regex.*;
 
 class Contact {
-	private String firstName;
-	private String lastName;
-	private String city;
-	private String state;
-	private String zip;
-	private String phoneNumber;
-	private String email;
+	private  String firstName;
+	private  String lastName;
+	private  String city;
+	private  String state;
+	private  String zip;
+	private  String phoneNumber;
+	private  String email;
 
 	public void setfirstName(String firstName) {
 		this.firstName = firstName;
@@ -64,33 +65,132 @@ class Contact {
 		return '\n'+"Contact is"+'\n'+ "First Name: " + firstName +'\n'+ "Last  Name: " + lastName +'\n'+ "City : " + city +'\n'+ "State : " + state
 				+ "zip : " + zip +'\n'+"Phone Number : " + phoneNumber +'\n'+ "Email Id: " + email +'\n';
 	}
+
+	Pattern p;
+	Matcher m;
+
+
+	public boolean isValidFirstName(){
+		String regex1="^[A-Z]{1}[a-z]+";
+		p = Pattern.compile(regex1);
+		m= p.matcher(getfirstName());
+		return m.matches();
+	}
+
+	public boolean isValidLastName() {
+		String regex="^[A-Z]{1}[A-Z a-z]{2,}$";
+		Pattern Q=Pattern.compile(regex);
+		Matcher n=Q.matcher(getlastName());
+		return n.matches();
+	}
+
+	public boolean isValidCity() {
+		String regex="^[A-Z]{1}[A-Z a-z]{2,}$";
+		Pattern Q=Pattern.compile(regex);
+		Matcher n=Q.matcher(getcity());
+		return n.matches();
+	}
+
+	public boolean isValidState() {
+		String regex="^[A-Z]{1}[A-Z a-z]{2,}$";
+		Pattern Q=Pattern.compile(regex);
+		Matcher n=Q.matcher(getstate());
+		return n.matches();
+	}
+
+	public boolean isValidZIP() {
+		String regex="^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";
+		Pattern Q=Pattern.compile(regex);
+		Matcher n=Q.matcher(getzip());
+		return n.matches();
+	}
+
+	public boolean isValidPhoneNumber() {
+		String regex="^[1-9]{2}\\s{0,1}[1-9]{1}[0-9]{9}$";
+		Pattern Q=Pattern.compile(regex);
+		Matcher n=Q.matcher(getphoneNumber());
+		return n.matches();
+	}
+
+	public boolean isValidEmailId() {
+		String regex="^[A-Z a-z 0-9]+([._+-][0-9 a-z A-Z]+)*@[0-9 a-z A-Z]+.[a-z A-Z]{2,3}([.][a-z A-Z]{2})*$";
+		Pattern Q=Pattern.compile(regex);
+		Matcher n=Q.matcher(getemail());
+		return n.matches();
+	}
 }
+
 
 public class Address_Book {
 	static String f_name;
 	static ArrayList<Contact> list=new ArrayList<Contact>();
 	static Scanner s=new Scanner(System.in);
 
+	//Add Contact
 	public void Add_Contact(Contact c) {
+
 		Scanner s=new Scanner(System.in);
 		System.out.print("Add Contact  \n");
-		System.out.print("Enter First Name: ");
-		c.setfirstName(s.nextLine());
-		System.out.print("Enter Last Name : ");
-		c.setlastName(s.nextLine());
-		System.out.print("Enter City : ");
-		c.setcity(s.nextLine());
-		System.out.print("Enter State : ");
-		c.setstate(s.nextLine());
-		System.out.print("Enter zip : ");
-		c.setzip(s.nextLine());
-		System.out.print("Enter PhoneNumber: ");
-		c.setphoneNumber(s.nextLine());
-		System.out.print("Enter Email ID : ");
-		c.setemail(s.nextLine());
+		while(true){
+			System.out.println("Enter First Name :");
+			c.setfirstName(s.nextLine());
+			if((c.isValidFirstName())==true)
+				break;
+			else
+				System.out.println("Invalid...1st letter must be Capital");
+		}
+		while(true) {
+			System.out.print("Enter Last Name : ");
+			c.setlastName(s.nextLine());
+			if((c.isValidLastName())==true)
+				break;
+			else
+				System.out.println("Invalid...1st letter must be Capital");
+		}
+		while(true) {
+			System.out.print("Enter City : ");
+			c.setcity(s.nextLine());
+			if((c.isValidCity())==true)
+				break;
+			else
+				System.out.println("Invalid...1st letter must be Capital");
+		}
+		while(true) {
+			System.out.print("Enter State : ");
+			c.setstate(s.nextLine());
+			if((c.isValidState())==true)
+				break;
+			else
+				System.out.println("Invalid...1st letter must be Capital");
+		}
+		while(true) {
+			System.out.print("Enter zip : ");
+			c.setzip(s.nextLine());
+			if((c.isValidZIP())==true)
+				break;
+			else
+				System.out.println("Invalid...must be 6 digits");
+		}
+		while(true) {
+			System.out.print("Enter PhoneNumber: ");
+			c.setphoneNumber(s.nextLine());
+			if((c.isValidPhoneNumber())==true)
+				break;
+			else
+				System.out.println("Invalid...must be 2 digit country code followed by 10 digit number");
+		}
+		while(true) {
+			System.out.print("Enter Email ID : ");
+			c.setemail(s.nextLine());
+			if((c.isValidEmailId())==true)
+				break;
+			else
+				System.out.println("Invalid...enter valid email Id");
+		}
 		list.add(c);
 	}
 
+	//Display All Contact
 	public void Display_All(){
 		boolean is_Empty=list.isEmpty();
 		if(is_Empty==true)
@@ -99,25 +199,69 @@ public class Address_Book {
 			System.out.println(list);
 	}
 
-	public void editContact(Contact c, String f_name ) {
+	public void editContact( String f_name) {
 		for (int i = 0; i < list.size(); i++) {
-			c = (Contact)list.get(i);
-			if (f_name.equals(c.getfirstName())) {
-				System.out.print("Enter new First Name: ");
-				c.setfirstName(s.nextLine());
-				System.out.print("Enter  new Last Name : ");
-				c.setlastName(s.nextLine());
-				System.out.print("Enter new City : ");
-				c.setcity(s.nextLine());
-				System.out.print("Enter new State : ");
-				c.setstate(s.nextLine());
-				System.out.print("Enter new zip : ");
-				c.setzip(s.nextLine());
-				System.out.print("Enter new PhoneNumber: ");
-				c.setphoneNumber(s.nextLine());
-				System.out.print("Enter new Email ID : ");
-				c.setemail(s.nextLine());
-				list.set(i,c);
+			if (list.get(i).getfirstName().equalsIgnoreCase(f_name)) {
+				Contact contact=list.get(i);
+
+				while(true){
+					System.out.println("Enter New First Name");
+					contact.setfirstName(s.next());
+					if((contact.isValidFirstName())==true)
+						break;
+					else
+						System.out.println("Invalid...1st letter must be Capital");
+				}
+				while(true) {
+					System.out.print("Enter New Last Name : ");
+					contact.setlastName(s.next());
+					if((contact.isValidLastName())==true)
+						break;
+					else
+						System.out.println("Invalid...1st letter must be Capital");
+				}
+				while(true) {
+					System.out.print("Enter New City : ");
+					contact.setcity(s.next());
+					if((contact.isValidCity())==true)
+						break;
+					else
+						System.out.println("Invalid...1st letter must be Capital");
+				}
+				while(true) {
+					System.out.print("Enter New State : ");
+					contact.setstate(s.next());
+					if((contact.isValidState())==true)
+						break;
+					else
+						System.out.println("Invalid...1st letter must be Capital");
+				}
+				while(true) {
+					System.out.print("Enter New zip : ");
+					contact.setzip(s.next());
+					if((contact.isValidZIP())==true)
+						break;
+					else
+						System.out.println("Invalid...must be 6 digits");
+				}
+				while(true) {
+					System.out.print("Enter New PhoneNumber: ");
+					contact.setphoneNumber(s.next());
+					if((contact.isValidPhoneNumber())==true)
+						break;
+					else
+						System.out.println("Invalid...must be 2 digit country code followed by 10 digit number");
+				}
+				while(true) {
+					System.out.print("Enter New Email ID : ");
+					contact.setemail(s.next());
+					if((contact.isValidEmailId())==true)
+						break;
+					else
+						System.out.println("Invalid...enter valid email Id");
+				}
+
+				list.set(i,contact);
 				System.out.println(list);
 			}
 			else
@@ -163,10 +307,10 @@ public class Address_Book {
 			case 3:
 				System.out.println("Enter first name that you want to edit:");
 				f_name=sc.nextLine();
-				book.editContact(c,f_name);
+				book.editContact(f_name);
 				break;
 			case 4:
-				System.out.println("Enter first name that you want to delete:");
+				System.out.println("Enter first name that you want to edit:");
 				f_name=sc.nextLine();
 				book.deleteContact(c,f_name);
 				break;
