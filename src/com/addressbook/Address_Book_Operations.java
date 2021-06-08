520763193cf36b76b6aa7512c087bca151fcd707
rehan7594 <rehansdm94@gmail.com>
2021-06-08 00:25:42 -0700

    Rfactor: code to ensure no duplicate entry of same person in particular…

---
+++ src/com/addressbook/Address_Book_Operations.java
@@ -0,0 +1,104 @@
+package com.addressbook;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+
+public class Address_Book_Operations {
+
+	static String f_name;
+	static Scanner s=new Scanner(System.in);
+
+	public static void addAddressBook(Map<String, List<Contact>> addressBooks) {
+		System.out.println("Add new AddressBook");
+		String bookName = s.next();
+		addressBooks.put(bookName, new ArrayList<Contact>());
+	}
+
+	public static void displayAllAddressBook(Map<String, List<Contact>> addressBooks) {
+		boolean is_Empty=addressBooks.isEmpty();
+		if(is_Empty==true)
+			System.out.println("No Address Books are Present");
+		else
+			System.out.println(addressBooks);
+	}
+
+	public static void editAddressBook(Map<String, List<Contact>> addressBooks) {
+		System.out.println("Enter the name of the AddressBook you want to Edit");
+		String bookName1 = s.next();
+
+		if(addressBooks.containsKey(bookName1)) {
+			List<Contact> contact = addressBooks.get(bookName1);
+			int choice;
+			do{
+				System.out.println("Enter your option : ");
+				System.out.println("1.Add contact  ");
+				System.out.println("2.Display all contact ");
+				System.out.println("3.Edit Contact ");
+				System.out.println("4.Delete Contact ");
+				System.out.println("5.Count of Total Contacts By City");
+				System.out.println("6.Count of Total Contacts By State");
+				System.out.println("7.Sorted Contacts by First Name ");
+				System.out.println("8.Sorted Contacts by City");
+				System.out.println("9.Sorted Contacts by State");	
+				System.out.println("10.Sorted Contacts by Zip");
+
+
+				choice=s.nextInt();
+				switch (choice)
+				{
+				case 1:
+					System.out.print("Add Contact  \n");
+					System.out.print("Enter First Name :");
+					String fName=s.next();
+
+					if (Contact_Operations.checkDuplicate(fName,contact)==false) {
+						Contact_Operations.Add_Contact(fName,contact);  
+					} else {
+						System.out.println("Already exists");
+					}
+					break;
+				case 2:			
+					Contact_Operations.Display_All(contact);
+					break;	
+				case 3:			
+					Contact_Operations.editContact(contact);
+					break;
+				case 4:
+					Contact_Operations.deleteContact(contact);
+					break;
+				case 5:
+					Contact_Operations.countByCity(contact);
+					break;
+				case 6:
+					Contact_Operations.countByState(contact);
+					break;	
+				case 7:
+					Contact_Operations.sortedContactByFirstName(contact);
+					break;
+				case 8:
+					Contact_Operations.sortedContactByCity(contact);
+					break;	
+				case 9:
+					Contact_Operations.sortedContactByState(contact);
+					break;
+				case 10:
+					Contact_Operations.sortedContactByZip(contact);
+					break;	
+				default:
+					System.out.println("Enter valid option");
+					break;	
+				}
+				System.out.println("Do you want to continue (1.Continue or 0.Exit)");
+				choice=s.nextInt();
+			}while(choice != 0);
+		}
+		else 
+			System.out.println("Invalid AddressBook Name!Not exist");
+	}
+
+
+
+}
---
+++ src/com/addressbook/Contact.java
@@ -0,0 +1,90 @@
+package com.addressbook;
+
+public class Contact {
+	private String firstName;
+	private String lastName;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNumber;
+	private String email;
+
+	public void setfirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getfirstName() {
+		return firstName;
+	}
+
+	public void setlastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getlastName() {
+		return lastName;
+	}
+
+	public void setcity(String city) {
+		this.city = city;
+	}
+	public String getcity() {
+		return city;
+	}
+
+	public void setstate(String state) {
+		this.state = state;
+	}
+	public String getstate() {
+		return state;
+	}
+
+	public void setzip(String zip) {
+		this.zip = zip;
+	}
+	public String getzip() {
+		return zip;
+	}
+
+	public void setphoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getphoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setemail(String email) {
+		this.email = email;
+	}
+	public String getemail() {
+		return email;
+	}
+
+	@Override
+	public String toString() {
+		return '\n'+"Contact is"+'\n'+ "First Name: " + firstName +'\n'+ "Last  Name: " + lastName +'\n'+ "City : " + city +'\n'+ "State : " + state
+				+ "\nzip : " + zip +'\n'+"Phone Number : " + phoneNumber +'\n'+ "Email Id: " + email +'\n';
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contact other = (Contact) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		return true;
+	}
+
+}
